3b7e4d026c936c1445050e3d91e7fb124a435231
Vinicius Accioly <vinicius.accioly@academico.ifpb.edu.br>
2022-06-01 11:10:15 -0300

    projeto coAp redes convergentes

---
+++ redes_convergentes/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt install vim screen htop cmake
+
+#Intalar o ambiente em python
+sudo apt install virtualenv python-all-dev python3-pip
+
+#Intalar a biblioteca CoAP em python
+
+sudo pip3 install aiocoap
+
+#Para executar o servidor CoAP
+
+#sudo pip install dtls
+sudo pip install python3-dtls
+
+sudo ln -s /usr/lib/x86_64-linux-gnu/libcrypto.so.1.0.0 /usr/lib/libcrypto.so.1.0.0
