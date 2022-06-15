00bb7066e112573108efe046d614a167d1a9b98e
Vinicius Accioly <vinicius.accioly@academico.ifpb.edu.br>
2022-06-14 22:32:23 -0300

    Fedora 36

---
+++ Tutoriais/pos-install-fedara.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Pos instalação Fedora
+sudo dnf update
+
+# Adicionando repositorios
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf config-manager --set-enable google-chrome
+
+# Programas de acesso remoto
+
+anydesk
+teamviewer
+
+# Programas em geral
+
+sudo dnf install google-chrome-stable vim terminator telegram-desktop obs-studio
+vscode
+
+discord
+
+
+
+# Virtualização https://fedorabr.org/discussion/414/tutorial-instalando-e-configurando-virtualbox-no-fedora-31-pelo-rpmfusion
+
+sudo dnf install kernel-devel-$(uname -r) akmod-VirtualBox
+sudo dnf install VirtualBox.x86_64
+sudo akmods
+sudo systemctl restart vboxdrv
+lsmod  | grep -i vbox
+
+
+
+VMware-workstation
